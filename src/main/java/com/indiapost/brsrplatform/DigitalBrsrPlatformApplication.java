858077e2697587c40c4cb74d037fecb0d63452ec
Karthick4521 <karthick4521@gmail.com>
2026-06-25 12:23:14 +0530

    Digital BRSR Platform for India Post -Complete

---
+++ src/main/java/com/indiapost/brsrplatform/DigitalBrsrPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.indiapost.brsrplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBrsrPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBrsrPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indiapost/brsrplatform/DigitalBrsrPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indiapost.brsrplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBrsrPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
